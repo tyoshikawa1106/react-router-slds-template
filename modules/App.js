afc558fe564d129cac36864067e1f5aa1a1f22d5
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-20 22:59:59 +0900

    initialize commit

---
+++ modules/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
+import Header from './Header'
+import NavLink from './NavLink'
+import Home from './Home'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Ghettohub Issues</h1>
+        <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
+        </ul>
+        <div>
+          {/* ... */}
+          {this.props.children || <Home/>}
+        </div>
+      </div>
+    )
+  }
+})
---
+++ modules/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return  <div className="slds-page-header" role="banner">
+              <div className="slds-grid">
+                <div className="slds-col slds-has-flexi-truncate">
+                  <p className="slds-text-heading--label">React Sample</p>
+                  <div className="slds-grid">
+                    <div className="slds-grid slds-type-focus slds-no-space">
+                      <h1 className="slds-page-header__title slds-truncate" title="React Router SLDS Template">React Router SLDS Template</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <p className="slds-text-body--small slds-page-header__info"></p>
+            </div>
+  }
+})
