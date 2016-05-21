a263014082b250393b2911bf552d8de3e2207f39
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-21 13:03:54 +0900

    routes.jsファイルの追加

--- index.js
+++ index.js
@@ -2,22 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import Home from './modules/Home'
-import About from './modules/About'
-import Repos from './modules/Repos'
-import Repo from './modules/Repo'
+import routes from './modules/routes'
 
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-
-      {/* add it here, as a child of `/` */}
-      <IndexRoute component={Home}/>
-
-      <Route path="/repos" component={Repos}>
-        <Route path="/repos/:userName/:repoName" component={Repo}/>
-      </Route>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
-), document.getElementById('app'))
+render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app')
+)
